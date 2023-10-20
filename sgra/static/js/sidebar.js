be8ebf3db1b547f9a0398016075b7661d3d6df9e
Davy Dantas <davy.dantas@escolar.ifrn.edu.br>
2023-10-20 13:31:59 -0300

    sidebar final

--- sgra/static/js/sidebar.js
+++ sgra/static/js/sidebar.js
@@ -1,4 +1,10 @@
 var menuItem = document.querySelectorAll('.item-menu');
+var dropdown = document.querySelectorAll('.dropdown');
+var navDrop = document.querySelectorAll('.nav-drop');
+var dropItem = document.querySelectorAll('.item-drop');
+var btnExp = document.querySelector('#btn-exp'); // Adicione "document." antes de querySelector
+var sidebar = document.querySelector('#aside'); // Adicione "document." antes de querySelector
+
 
 function selectLink(event) {
     event.preventDefault(); // Evita o comportamento padrão do link
@@ -7,15 +13,47 @@ function selectLink(event) {
         item.classList.remove('ativo')
     )
     this.classList.add('ativo');
+
 }
 
 menuItem.forEach((item) =>
     item.addEventListener('click', selectLink)
 )
 
-var btnExp = document.querySelector('#btn-exp'); // Adicione "document." antes de querySelector
-var sidebar = document.querySelector('#aside'); // Adicione "document." antes de querySelector
+function selectLinkDrop(_event) {
+
+    dropItem.forEach((item) =>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo');
+}
+
+dropItem.forEach((item) =>
+    item.addEventListener('click', selectLinkDrop)
+)
 
 btnExp.addEventListener('click', function() {
     sidebar.classList.toggle('reduce');
+    
+    dropdown.forEach(function(item) {
+        if (item.classList.contains('down')) {
+            item.classList.remove('down');
+        }
+    });
+});
+
+navDrop.forEach(function(navDrop) {
+    navDrop.addEventListener('click', function(event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        
+        // Obtém o elemento 'li' pai do elemento 'a' clicado
+        var drop = this.closest('li.dropdown');
+        // Adiciona a classe 'down' ao 'dropdown' associado
+        drop.classList.toggle('down');
+
+        if(sidebar.classList.contains('reduce')){
+            sidebar.classList.remove('reduce');
+        }
+
+    });
 });
